c3be75a3566b0815401b86a4291cce85bc7f88da
Ken Liu <ken.liu1@ucalgary.ca>
2023-02-08 12:01:31 -0700

    Updated to use memoization
    - Includes a dictionary to store previous results

--- ex1.3.py
+++ ex1.3.py
@@ -1,7 +1,10 @@
+prev_result = {}
 
 def func(n):
     if n == 0 or n == 1:
         return n
     
-    else:  
-        return func(n-1) + func(n-2)
+    if n not in prev_result:
+        prev_result[n] = func(n - 1) + func(n -2)
+    
+    return prev_result[n]
