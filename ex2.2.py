544f06879b90548253833764de5e4c5e9f42595b
HylianMuffins <catholiczach@gmail.com>
2023-02-08 11:30:10 -0700

    Added templates for the questions with code.

---
+++ ex2.2.py
@@ -0,0 +1,5 @@
+# Test the code on all the inputs at:
+# https://raw.githubusercontent.com/ldklab/ensf338w23/main/assignme
+# nts/assignment2/ex2.json
+# Plot timing results.
+
---
+++ ex2.4.py
@@ -0,0 +1,2 @@
+# Change the code – if possible – to improve its performance on the input given in point 2.
+
