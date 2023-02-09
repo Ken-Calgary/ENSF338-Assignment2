97f7abaee8870c69fa33b8c47a9e7790a66495a2
HylianMuffins <catholiczach@gmail.com>
2023-02-09 13:19:25 -0700

    added the ex2.txt file for testing

--- ex2.2.py
+++ ex2.2.py
@@ -1,6 +1,5 @@
 # Test the code on all the inputs at:
-# https://raw.githubusercontent.com/ldklab/ensf338w23/main/assignme
-# nts/assignment2/ex2.json
+# https://raw.githubusercontent.com/ldklab/ensf338w23/main/assignments/assignment2/ex2.json
 # Plot timing results.
 
 import sys
